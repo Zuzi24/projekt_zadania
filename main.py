2c019e72902b74d9f6760babef3e9804c6d8f302
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-05 22:44:58 +0200

    wip: add new remove station functionality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.model import research_stations
-from utils.controller import get_stations_info, add_station
+from utils.controller import get_stations_info, add_station, remove_station
 
 
 def main():
@@ -7,12 +7,14 @@ def main():
     print("0 - zakończ program")
     print("1 - wyświetl dane stacji badawczych")
     print("2 - dodaj stacje badawczą")
+    print("3 - usuń stacje badawczą")
     print("===========================")
     while True:
         choice: str = input("wybierz opcje menu ")
         if choice == "0": break
         if choice == "1": get_stations_info(research_stations)
         if choice == "2": add_station(research_stations)
+        if choice == "3": remove_station(research_stations)
 
 
 if __name__ == "__main__":
--- utils/controller.py
+++ utils/controller.py
@@ -9,3 +9,10 @@ def add_station(stations_data : list ) -> None:
     new_station: str = input("podaj nazwe nowej stacji badawczą")
     new_location: str = input("podaj lokalizacje")
     stations_data.append({"name": new_station, "location": new_location})
+
+
+def remove_station(stations_data : list ) -> None:
+    station_name: str = input("podaj nazwe stacji badawczej do usunięcia")
+    for station in stations_data:
+        if station["name"] == station_name:
+            stations_data.remove(station)
