eceee684263a0765fcce194f14ed669594fcb3f0
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-05 21:44:37 +0200

    chore: the code was restructured according to the mvc template

--- main.py
+++ main.py
@@ -1,17 +1,10 @@
+from utils.model import research_stations
+from utils.controller import get_stations_info
 
 
-research_stations : list =[
-
-    {"name" : "Instytut_Centrum_Zdrowia_Matki_Polki" , "location": "Łódź" },
-    {"name" : "Instytut_Fizjologii_i_Patologii_Słuchu" , "location": "Warszawa"},
-    {"name" : "Instytut_Chemicznej_Przeróbki_Węgla" , "location": "Zabrze"},
-    {"name" : "Instytut_Wymiaru_Sprawiedliwości" , "location": "Warszawa" },
-
-]
-
-def get_stations_info( stations_data : list ) -> None:
-    for station in stations_data:
-        print(f"{station['name']} : {station['location']}" )
+def main():
+    get_stations_info(research_stations)
 
 
-get_stations_info( research_stations )
+if __name__ == "__main__":
+    main()
