e3531af00b9b9644342c96ad9f5c9887ab1c8efc
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-05 22:34:23 +0200

    wip: add new station functionality

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from utils.model import research_stations
-from utils.controller import get_stations_info
+from utils.controller import get_stations_info, add_station
 
 
 def main():
     print("=========MENU========")
     print("0 - zakończ program")
     print("1 - wyświetl dane stacji badawczych")
+    print("2 - dodaj stacje badawczą")
     print("===========================")
     while True:
         choice: str = input("wybierz opcje menu ")
         if choice == "0": break
-        elif choice == "1": get_stations_info(research_stations)
+        if choice == "1": get_stations_info(research_stations)
+        if choice == "2": add_station(research_stations)
 
 
-main()
+if __name__ == "__main__":
+    main()
--- utils/controller.py
+++ utils/controller.py
@@ -2,3 +2,10 @@
 def get_stations_info( stations_data : list ) -> None:
     for station in stations_data:
         print(f"{station['name']} : {station['location']}" )
+
+
+
+def add_station(stations_data : list ) -> None:
+    new_station: str = input("podaj nazwe nowej stacji badawczą")
+    new_location: str = input("podaj lokalizacje")
+    stations_data.append({"name": new_station, "location": new_location})
