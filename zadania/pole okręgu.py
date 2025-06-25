493dd5d27a8fa068c1e30b8eddaab477fbc51618
Zuzi2810 <zuzannamierkiewicz4@gmail.com>
2025-06-25 09:31:27 +0200

    nowe

---
+++ zadania/pole okręgu.py
@@ -0,0 +1,11 @@
+#pole okręgu na danych od użytkownika
+
+r=float(input("podaj promień okręgu: "))    #float-że będzie to liczba z przecinkiem
+                                            #podanie promienia
+
+r2 = pow(r,2)                               #Funkcja pow(a, b) oznacza "a do potęgi b"
+
+pole=r2*3.14                                #pod wzór na pole
+
+print(pole)                                 #pokaż wynik
+
